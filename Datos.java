c20bce92df70efc82742875223e7b2a9c21e9f49
gabyordonez <37216839+gabyordonez@users.noreply.github.com>
2018-03-17 08:15:09 -0600

    Add files via upload

---
+++ Datos.java
@@ -0,0 +1,26 @@
+
+package datos;
+
+/**
+ *
+ * @author America Gabriela Ordoñez tobias
+ * carnet: 00361516
+ */
+public class Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombre: Gabriela Ordoñez");
+        System.out.println( "Direccion: Col. Escalon");
+        System.out.println("Cancion favorita: Arsenico");
+        System.out.println("colegio: La Floresta");
+        System.out.println("Carrera: Ing. Informatica");
+        System.out.println("Edad: 20");
+        System.out.println("Celular: 77438880");
+        System.out.println("Carnet: 00361516");
+        System.out.println( "Año: 1998");
+    }
+    
+}
